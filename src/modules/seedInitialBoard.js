6428463e39d08dfd92db2567c24209e949769d54
Corrine Morita <corrine@promptt.com>
2018-05-16 10:11:02 -0700

    Add more boards
    Co-authored-by: JBallin <JBallin@users.noreply.github.com>

--- src/modules/seedInitialBoard.js
+++ src/modules/seedInitialBoard.js
@@ -1,6 +1,20 @@
-const initialBoard = [
+const board1 = [
   ['R', 'H', 'R', 'E'],
   ['Y', 'P', 'C', 'S'],
   ['W', 'N', 'S', 'N'],
   ['T', 'E', 'G', 'O']
+];
+
+const board2 = [
+  ['S', 'P', 'O', 'U'],
+  ['T', 'A', 'Y', 'O'],
+  ['W', 'N', 'K', 'I'],
+  ['A', 'X', 'D', 'N']
+];
+
+const board3 = [
+  ['U', 'T', 'A', 'D'],
+  ['J', 'N', 'M', 'E'],
+  ['T', 'S', 'E', 'A'],
+  ['N', 'E', 'D', 'P']
 ];
