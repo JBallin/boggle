5a53ca9eb3a5d04c504c22836bf87443ea101669
Corrine Morita <corrine@promptt.com>
2018-05-16 11:26:00 -0700

    Clean up app.js
    Co-authored-by: JBallin <JBallin@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
-// - Create new “submissionLetters” array
 // - On click, Verify adjacent/diagonal move (create separate verifyMove function). If not, reset submission. If yes push letter (element.innerHTML) as an integer to “submission”
 // - highlight box background color/border of selected words
 // - Display submission progress
-// - Convert box id’s into indexes
 // - Verify word not in usedWords -> error or add to usedWords and update display of usedWords. make sure readme explains no duplicates
 // - FIRST LETTER onclick, how does it know its first letter? because submissionIndexes is empty? push letter from to submissionLetters
 // - SECOND LETTER box.onclick, check if validMove (which calls the convertId...) ? submissionIDs.push(this.id) submissionLetters.push(querySelector(#id (how get ID?) > p).innerHTML) : alert(invalid error - with reason why (3+ or adj/diag)?) + resetSubmissionArray(s)AndBoxHighlighting (write resetSubmissionArray(s)... function? needed somewhere else? yes! for submit button)
 // - check if duplicate not in usedWords array? submit button needs to concat the letters array and push to usedWords array..how do that again? resetSubmissionArrays() : error(duplicate) resetSubmissionArrays()
 // - display usedWords
-// - add to loop: newBox.onclick = "makeMove(this)"
-// - reset function to clear data??
+// - box onclick remove selectLetter function, so you can't click a previously clicked box
+//    - resetSubmission should reset onclicks
 // - create wordsUsed HTML
-// - test if more than 3+ characters
-
-// - function convertIdToIndices(id) that returns an array of [rowIndex, boxIndex]
-// - write selectLetter that calls isValidMove which calls convertIdToIndices etc.
 
 //==================================================
 
@@ -62,36 +56,6 @@ function isValidMove(boxNum) {
   // test if valid with Math.abs from toy problem
 
 }
-
-//===============================================================
-const game1 = {
-  board: [
-    ['a', 'c', 'r'],
-    ['b', 'g', 'k'],
-    ['e', 't', 'j']
-  ],
-};
-
-function isValidAdjacentMove(cur_letter, next_letter) {
-  return [0,1].includes(Math.abs(cur_letter[0] - next_letter[0])) &&
-    [0, 1].includes(Math.abs(cur_letter[1] - next_letter[1]));
-}
-
-function makeMove(game, move) {
-  let word = '';
-  let num_letters = move.length;
-
-for (let i = 0; i < num_letters - 1; i++) {
-  let cur_letter = move[i];
-  let next_letter = move[i+1];
-  if (!isValidAdjacentMove(cur_letter, next_letter)) {
-    return 'invalid: move must be adjacent or diagonal';
-  }
-  word += game.board[cur_letter[0]][cur_letter[1]];
-  if (i === num_letters - 2) word += game.board[next_letter[0]][next_letter[1]];
-}
-
-//===============================================================
 
 function submitWord() {
   newWord = submittedLetters.join('');
